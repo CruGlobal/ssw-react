4a4be688b9408d3fb804f4377a533865b3933429
Bill Randall <william.randall@cru.org>
2020-03-16 11:43:41 -0400

    Set up clientlib creation

---
+++ clientlib.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/StaffWeb/components/page/editable/htmlbase/clientlibs/react",
+
+    libs: {
+        name: "staffweb-react-app",
+        allowProxy: true,
+        categories: ["staffweb.react"],
+        serializationFormat: "xml",
+        jsProcessor: ["min:gcc"],
+        assets: {
+            js: [
+                "build/static/**/*.js"
+            ],
+            css: [
+                "build/static/**/*.css"
+            ]
+        }
+    }
+};
