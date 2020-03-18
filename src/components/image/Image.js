8ea0320dda0d860a4c182044df0974009bcb5fe3
Bill Randall <william.randall@cru.org>
2020-03-18 13:29:41 -0400

    Map the image component to the AEM image component

---
+++ src/components/image/Image.js
@@ -0,0 +1,38 @@
+/*
+ * Maps to CruOrgApp/components/content/image
+ */
+import React, {Component} from 'react';
+import {MapTo} from '@adobe/cq-react-editable-components';
+
+/**
+* Default Edit configuration for the Image component that interact with the Core Image component and sub-types
+*
+* @type EditConfig
+*/
+const ImageEditConfig = {
+
+    emptyLabel: 'Image',
+
+    isEmpty: function(props) {
+        return !props || !props.src || props.src.trim().length < 1;
+    }
+};
+
+/**
+ * Image React component
+ */
+class Image extends Component {
+
+    get content() {
+        return <img src={this.props.src} alt={this.props.alt}
+            title={this.props.displayPopupTitle && this.props.title}/>
+    }
+
+    render() {
+        return (<div className="Image">
+                {this.content}
+            </div>);
+    }
+}
+
+MapTo('CruOrgApp/components/content/image')(Image, ImageEditConfig);
