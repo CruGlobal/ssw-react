6d0eb3abc52904f734b277794e4e72a127c5d20d
Bill Randall <william.randall@cru.org>
2020-03-30 13:54:17 -0400

    Handle placeholder for form textfield

--- src/components/form/text/FormText.js
+++ src/components/form/text/FormText.js
@@ -25,9 +25,13 @@ export default class FormText extends Component {
     render() {
         let label = !this.props.hideLabel ? (<label htmlFor={this.props.name}>{this.props.title}</label>) : '';
         let input = (
-            <input type={this.props.type} className="FormText" name={this.props.name} defaultValue={this.props.value} />
+            <input type={this.props.type}
+                   className="FormText"
+                   name={this.props.name}
+                   defaultValue={this.props.value}
+                   placeholder={this.props.placeholder} />
         );
-        let toolTip = this.props.helpMessage ?
+        let toolTip = this.props.helpMessage && !this.props.placeholder ?
             (<p className="cmp-form-text__help-block">{this.props.helpMessage}</p>) : '';
 
         return (
